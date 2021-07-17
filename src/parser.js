c47cd2d8747969423f2d1c5fcbb629dda67ffbfd
TNSahid <sahid.miller@gmail.com>
2021-07-17 17:32:49 -0400

    Initial commit

---
+++ src/parser.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const utils = require("./utils");
+
+module.exports = (yargs) =>
+  yargs
+    .option("silent", {
+      desc: "Write no output",
+      type: "boolean",
+      default: false,
+      coerce: (silent) => {
+        if (silent) utils.disablePrinting();
+        return silent;
+      },
+    })
+    .option("pass", {
+      desc: "Pass phrase for the keys",
+      type: "string",
+      default: "",
+    })
+    .option("migrate", {
+      desc: "Enable/disable automatic repo migrations",
+      type: "boolean",
+      default: false,
+    })
+    .options("api", {
+      desc: "Remote API multiaddr to use",
+      type: "string",
+    })
+    .epilog(utils.ipfsPathHelp)
+    .demandCommand(1, "Please specify a command")
+    .showHelpOnFail(false)
+    .commandDir("commands")
+    .help()
+    .strict()
+    .completion();
